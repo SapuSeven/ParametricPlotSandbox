5fdf88c8c4cf15942c61c45a37057e97eedccdea
SapuSeven <sapuseven@gmail.com>
2017-11-07 21:35:59 +0100

    Initial commit

---
+++ src/CanvasPanel.java
@@ -0,0 +1,117 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class CanvasPanel extends JPanel {
+	private Timer timer;
+	private double t = 0;
+	private final ArrayList<Point> points = new ArrayList<>();
+	private SandboxFrame frame;
+	private int deletedPoints = 0;
+	private int fps = 60;
+	private double increment = 0.05;
+	private final ActionListener timerAction = e -> redraw();
+	private double n1 = 1, n2 = 1;
+	private final int scale = 250;
+	private int maxPoints = 1000;
+	private float strokeWidth = 3;
+	private boolean rainbow;
+
+	void init(SandboxFrame frame) {
+		this.frame = frame;
+
+		int timerDelay = 1000 / fps;
+		timer = new Timer(timerDelay, timerAction);
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		Graphics2D g2d = (Graphics2D) g;
+
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+				RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+
+		g2d.setColor(Color.WHITE);
+		g2d.setStroke(new BasicStroke(strokeWidth));
+
+		points.add(new Point((int) Math.round(scale * Math.sin(n1 * t)), (int) Math.round(scale * Math.sin(n2 * t))));
+		if (points.size() > 1) {
+			while (points.size() >= maxPoints) {
+				points.remove(0);
+				deletedPoints++;
+			}
+			for (int i = 1; i < points.size(); i++) {
+				Point p1 = points.get(i - 1);
+				Point p2 = points.get(i);
+				if (rainbow)
+					g2d.setColor(Color.getHSBColor((i + deletedPoints) * 0.001f, 1, 1));
+				g2d.drawLine(
+						(int) Math.round(p1.getX()) + getWidth() / 2, (int) Math.round(p1.getY()) + getHeight() / 2,
+						(int) Math.round(p2.getX()) + getWidth() / 2, (int) Math.round(p2.getY()) + getHeight() / 2
+				);
+			}
+		}
+
+		g2d.dispose();
+	}
+
+	private void redraw() {
+		t += increment;
+		repaint();
+	}
+
+	void setN1(double n1) {
+		this.n1 = n1;
+		reset();
+	}
+
+	void setN2(double n2) {
+		this.n2 = n2;
+		reset();
+	}
+
+	void setMaxPoints(int maxPoints) {
+		this.maxPoints = maxPoints;
+	}
+
+	void reset() {
+		t = 0;
+		points.clear();
+		deletedPoints = 0;
+		if (!timer.isRunning())
+			repaint();
+	}
+
+	void setStrokeWidth(float strokeWidth) {
+		this.strokeWidth = strokeWidth;
+	}
+
+	void startStop() {
+		if (timer.isRunning()) {
+			timer.stop();
+			frame.setStartButtonText("Start");
+		} else {
+			timer.start();
+			frame.setStartButtonText("Stop");
+		}
+	}
+
+	void setIncrement(double increment) {
+		this.increment = increment;
+	}
+
+	void setFps(int fps) {
+		this.fps = fps;
+
+		timer.setDelay(1000 / fps);
+	}
+
+	void setRainbow(boolean rainbow) {
+		this.rainbow = rainbow;
+	}
+}
---
+++ src/SandboxFrame.java
@@ -0,0 +1,70 @@
+import javax.swing.*;
+import java.awt.*;
+
+class SandboxFrame {
+	private JPanel root;
+	private CanvasPanel canvas;
+	private JButton start;
+	private JSpinner n1spinner;
+	private JSpinner n2spinner;
+	private JSpinner maxPoints;
+	private JSlider strokeWidth;
+	private JCheckBox rainbow;
+	private JButton reset;
+	private JSpinner increment;
+	private JSpinner fps;
+
+	private final String title;
+	private final int width;
+	private final int height;
+
+	SandboxFrame(String title, int width, int height) {
+		this.title = title;
+		this.width = width;
+		this.height = height;
+
+		createDisplay();
+	}
+
+	// Setup all GUI elements
+	private void createDisplay() {
+		JFrame frame = new JFrame(title);
+
+		// Sliders & Spinners (top section)
+		n1spinner.setModel(new SpinnerNumberModel(1.0, 0.01, 10.0, 0.01));
+		n1spinner.addChangeListener(e -> canvas.setN1((double) n1spinner.getValue()));
+		n2spinner.setModel(new SpinnerNumberModel(1.0, 0.01, 10.0, 0.01));
+		n2spinner.addChangeListener(e -> canvas.setN2((double) n2spinner.getValue()));
+		maxPoints.setModel(new SpinnerNumberModel(1000, 3, 10000, 1));
+		maxPoints.addChangeListener(e -> canvas.setMaxPoints((int) maxPoints.getValue()));
+		strokeWidth.addChangeListener(e -> canvas.setStrokeWidth(strokeWidth.getValue() / 10));
+		increment.setModel(new SpinnerNumberModel(0.05, 0.001, 1, 0.01));
+		increment.addChangeListener(e -> canvas.setIncrement((double) increment.getValue()));
+		fps.setModel(new SpinnerNumberModel(60, 1, 1000, 1));
+		fps.addChangeListener(e -> canvas.setFps((int) fps.getValue()));
+		rainbow.addActionListener(e -> canvas.setRainbow(rainbow.isSelected()));
+
+		// Buttons (bottom section)
+		start.addActionListener(e -> canvas.startStop());
+		reset.addActionListener(e -> canvas.reset());
+
+		// The canvas is for plotting the function
+		canvas.setPreferredSize(new Dimension(width, height));
+		canvas.setMaximumSize(new Dimension(width, height));
+		canvas.setMinimumSize(new Dimension(width, height));
+		canvas.setFocusable(false);
+		canvas.setBackground(Color.BLACK);
+		canvas.init(this);
+
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setContentPane(root);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+	void setStartButtonText(String text) {
+		start.setText(text);
+	}
+}
